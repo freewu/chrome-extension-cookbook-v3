4bdeb393ad67b2890e4d5964e0f32c09e75565c2
bluefrog <bluefrog.wu@gmail.com>
2025-11-05 11:52:44 +0800

    docsify to pdf 有  toc 的影响

---
+++ .docsifytopdfrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  contents: [ "_sidebar.md" ], // array of "table of contents" files path
+  pathToPublic: "chrome-extension-cookbook-v3.pdf", // path where pdf will stored
+  pdfOptions: "<options for puppeteer.pdf()>", // reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
+  removeTemp: true, // remove generated .md and .html or not
+  emulateMedia: "print", // mediaType, emulating by puppeteer for rendering pdf, 'print' by default (reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageemulatemediamediatype)
+}
